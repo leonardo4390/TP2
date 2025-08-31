a2fb985f391c1e50037ddd21e0b47f87d2d3ed27
leoardo4390 <tidus4390@gmail.com>
2025-08-30 22:56:19 -0300

    modificando mi principal para adaptarse a lo pedido

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ class Program
 
         Cadeteria cadeteria = new Cadeteria(rutaCadeteria, rutaCadetes);
         GestioPedidos(cadeteria);
-        
+
     }
 
     public static void GestioPedidos(Cadeteria cadeteria)
@@ -20,8 +20,8 @@ class Program
             Console.WriteLine("1. Dar de alta pedido");
             Console.WriteLine("2. Asignar pedido a cadete");
             Console.WriteLine("3. Cambiar estado de pedido");
-            Console.WriteLine("4. Reasignar pedido a otro cadete");
-            Console.WriteLine("5. Mostrar informe");
+            Console.WriteLine("4. Mostrar Pedido");
+            Console.WriteLine("5. Calcular Jornal Cadete");
             Console.WriteLine("6. Salir");
 
             Console.Write("Seleccione una opción: ");
@@ -33,16 +33,16 @@ class Program
                     cadeteria.DarDeAltaPedido();
                     break;
                 case "2":
-                    cadeteria.AsignarPedidoACadete();
+                    AsignarCadete(cadeteria);
                     break;
                 case "3":
                     cadeteria.CambiarEstadoPedido();
                     break;
                 case "4":
-                    cadeteria.ReasignarPedido();
+                    cadeteria.MostrarInforme();
                     break;
                 case "5":
-                    cadeteria.MostrarInforme();
+                    CalcularJornal(cadeteria);
                     break;
                 case "6":
                     Console.WriteLine("Cerrando sistema...");
@@ -53,5 +53,50 @@ class Program
                     break;
             }
         }
+    }
+
+    public static void AsignarCadete(Cadeteria cadeteria)
+    {
+        Console.WriteLine("\nPedidos Disponibles:");
+        foreach (var p in cadeteria.Pedidos)
+        {
+            string estadoCadete = p.CadeteAsignado != null ? $"asignado a {p.CadeteAsignado.Nombre}" : "Sin asignar";
+            Console.WriteLine($"Pedido: {p.Nro} - Cliente: {p.Datocliente.Nombre}");
+        }
+
+        Console.WriteLine("Ingrese el numero de pedido: ");
+        if (!int.TryParse(Console.ReadLine(), out int nroPedido))
+        {
+            Console.WriteLine("Numero no Valido.");
+            return;
+        }
+
+        Console.WriteLine("\nCadetes Disponibles:");
+        foreach (var c in cadeteria.Cadetes)
+        {
+            Console.WriteLine($"Id: {c.Id} - {c.Nombre}");
+        }
+
+        Console.WriteLine("Ingrese Id de Cadete: ");
+        if (!int.TryParse(Console.ReadLine(), out int idCadete))
+        {
+            Console.WriteLine("Id no Valido.");
+            return;
+        }
+
+        cadeteria.AsignarCadeteAPedido(idCadete, nroPedido);
+    }
+
+    public static void CalcularJornal(Cadeteria cadeteria)
+    {
+        Console.WriteLine("\nIngrese el Id del cadete: ");
+        if (!int.TryParse(Console.ReadLine(), out int id))
+        {
+            Console.WriteLine("Id No Valido.");
+            return;
+        }
+
+        int jornal = cadeteria.JornalACobrar(id);
+        Console.WriteLine($"Jornal a Cobrar: {jornal}");
     }
 }
