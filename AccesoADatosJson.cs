6b0fc0a90b00ca6ce9fffd36c09a284e218dd47e
leoardo4390 <tidus4390@gmail.com>
2025-08-31 19:55:39 -0300

    creando Acceso a datos Json

---
+++ AccesoADatosJson.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text.Json;
+
+
+class AccesoADatosJson : IAccesoADatos
+{
+    public string[] LeerCadeteria(string ruta)
+    {
+        var json = File.ReadAllText(ruta);
+        var datos = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+        return new[] { datos["nombre"], datos["telefono"] };
+    }
+
+    public List<string[]> LeerCadetes(string ruta)
+    {
+        var json = File.ReadAllText(ruta);
+        var cadetes = JsonSerializer.Deserialize<List<Dictionary<string, string>>>(json);
+        var resultado = new List<string[]>();
+
+        foreach (var cadete in cadetes)
+        {
+            resultado.Add(new[] {
+                cadete["nombre"],
+                cadete["direccion"],
+                cadete["telefono"]
+            });
+        }
+
+        return resultado;
+    }
+
+}
