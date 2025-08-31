4535ce77d0eb88b943efc80e0c195c349bd3800f
leoardo4390 <tidus4390@gmail.com>
2025-08-30 21:50:15 -0300

    modificando pedido de acuerdo a lo pedido

--- Pedido.cs
+++ Pedido.cs
@@ -8,6 +8,7 @@ class Pedido
     public Cliente Datocliente { get; private set; }
     public string Estado { get; set; }
     public List<Producto> Productos { get; private set; }
+    public Cadete CadeteAsignado{ get; private set; }
 
     public Pedido(int nro, string obs, Cliente cliente, string estado)
     {
@@ -18,29 +19,18 @@ class Pedido
         Productos = new List<Producto>();
     }
 
-    public void AgregarProducto(Producto producto)
-    {
-        Productos.Add(producto);
-    }
+    public void AgregarCadete(Cadete cadete) => CadeteAsignado = cadete;
 
-    public void VerDireccionCliente()
-    {
-        Console.WriteLine($"Dirección: {Datocliente.Direccion}");
-        Console.WriteLine($"Referencia: {Datocliente.ReferenciaDireccion}");
-    }
+    public void AgregarProducto(Producto producto) => Productos.Add(producto);
 
-    public void VerInformacionCliente()
-    {
-        Console.WriteLine($"Cliente: {Datocliente.Nombre}");
-        Console.WriteLine($"Teléfono: {Datocliente.Telefono}");
-    }
+    public bool EstaEntregado() => Estado.ToLower() == "entregado";
 
     public void MostrarTicket()
     {
         Console.WriteLine("\nTICKET DEL PEDIDO");
         Console.WriteLine($"Pedido N°: {Nro}");
-        VerInformacionCliente();
-        VerDireccionCliente();
+        Console.WriteLine($"Cliente: {Datocliente.Nombre} - Tel: {Datocliente.Telefono}");
+        Console.WriteLine($"Direccion: {Datocliente.Direccion} - Ref: {Datocliente.ReferenciaDireccion}");
         Console.WriteLine($"Observación: {Obs}");
         Console.WriteLine($"Productos:");
         foreach (var producto in Productos)
@@ -48,12 +38,8 @@ class Pedido
             Console.WriteLine($"- {producto}");
         }
         Console.WriteLine($"Estado: {Estado}");
+        Console.WriteLine($"Cadete Asignado: {(CadeteAsignado != null? CadeteAsignado.Nombre : "Sin Asignaar")}");
         Console.WriteLine("-----------------------------\n");
-    }
-
-    public bool EstaEntregado()
-    {
-        return Estado.ToLower() == "entregado";
     }
 
 }
