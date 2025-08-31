538e54560c297c4f60f21e3efce171d061253d92
leoardo4390 <tidus4390@gmail.com>
2025-08-31 19:40:11 -0300

    refactorizando CSV a AccesoADatosCSV

---
+++ AccesoADatosCSV.cs
@@ -0,0 +1,25 @@
+using System;
+
+class AccesoADatosCSV : IAccesoADatos
+{
+    public string[] LeerCadeteria(string ruta)
+    {
+        using var leer = new StreamReader(ruta);
+        leer.ReadLine();
+        return leer.ReadLine()?.Split(',');
+    }
+
+    public List<string[]> LeerCadetes(string ruta)
+    {
+        var datos = new List<string[]>();
+        using var leer = new StreamReader(ruta);
+        leer.ReadLine();
+        while (!leer.EndOfStream)
+        {
+            var linea = leer.ReadLine();
+            if (!string.IsNullOrWhiteSpace(linea))
+                datos.Add(linea.Split(','));
+        }
+        return datos;
+    }
+}
