2c70ef711afef468a28d243d91239944511306a2
leoardo4390 <tidus4390@gmail.com>
2025-08-30 21:30:56 -0300

    modificando Cadete y enlazando con Persona

--- Cadete.cs
+++ Cadete.cs
@@ -1,33 +1,13 @@
 using System;
 using System.Collections.Generic;
 
-class Cadete
+class Cadete: Persona
 {
     private static int idCadete = 0;
 
     public int Id { get; private set; }
-    public string Nombre { get; private set; }
-    public string Direccion { get; private set; }
-    public int Telefono { get; private set; }
-    public List<Pedido> Pedidos { get; private set; }
-
-    public Cadete(string nombre, string direccion, int telefono)
+    public Cadete(string nombre, string direccion, int telefono): base(nombre, direccion, telefono)
     {
         Id = ++idCadete;
-        Nombre = nombre;
-        Direccion = direccion;
-        Telefono = telefono;
-        Pedidos = new List<Pedido>();
     }
-
-    public void AgregarPedido(Pedido pedido)
-    {
-        Pedidos.Add(pedido);
-    }
-
-    public void EliminarPedido(int nro)
-    {
-        Pedidos.RemoveAll(p => p.Nro == nro);
-    }
-
 }
